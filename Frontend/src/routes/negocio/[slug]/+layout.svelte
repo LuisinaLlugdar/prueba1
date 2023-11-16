b1b7c200cd431480856679387a2d26196a7beee7
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-11-16 15:56:32 +0000

    prueba

---
+++ Frontend/src/routes/negocio/[slug]/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Header from "../../../components/privateHeader.svelte";
+  import Footer from "../../../components/publicFooter.svelte";
+</script>
+
+<Header />
+<div class="py-10 ">
+  <slot />
+</div>
+<Footer />
